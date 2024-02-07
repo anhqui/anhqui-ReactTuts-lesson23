fa1fa2f609d8a8683ef37c33e8aa2508f63538c7
Qui Nguyen <qui@gmail.com>
2024-02-06 22:37:45 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,22 @@
+import { useState } from "react";
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("hello, john", e);
+  // let name = "john";
+  const [name, setName] = useState("john");
+  const [age, setAge] = useState(30);
+  const handleClick = () => {
+    // name = "maria";
+    // console.log(name);
+    setName("maria");
+    setAge(25);
   };
 
-  const handleClickAgain = (name, e) => {
-    console.log("hello " + name, e.target);
-  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("maria", e)}>
-        Click me again
-      </button>
     </div>
   );
 };
