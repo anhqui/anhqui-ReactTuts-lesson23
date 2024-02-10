705d0062eb5bcbfa1e5a6e6253d031fc2a914284
Qui Nguyen <qui@gmail.com>
2024-02-10 11:25:08 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -26,6 +26,12 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("use effect run");
+    console.log(blogs);
+  });
+
   return (
     <div className="home">
       <BlogList
