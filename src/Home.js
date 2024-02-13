f9d34a5aae24266bbfd6d2077dd6002e9921c1e5
Qui Nguyen <qui@gmail.com>
2024-02-12 22:35:13 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,23 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
-      .then((data) => {
-        setBlogs(data);
-      });
+    // simulate 1s of delay
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => res.json())
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} heading="All Blogs!" />}
     </div>
   );
