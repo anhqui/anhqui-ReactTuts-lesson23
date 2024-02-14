1449bf35b3b6ceb1585efb315a10a364f5492107
Qui Nguyen <qui@gmail.com>
2024-02-13 23:05:45 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // simulate 1s of delay
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          // console.log(res);
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          // console.log(err.message);
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <div>{error}</div>}
