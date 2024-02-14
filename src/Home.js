0ec79bac07060f59cd9566b7ebe5463a822a718f
Qui Nguyen <qui@gmail.com>
2024-02-13 21:31:56 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,35 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // simulate 1s of delay
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-        .then((res) => res.json())
+        .then((res) => {
+          // console.log(res);
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          // console.log(err.message);
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} heading="All Blogs!" />}
     </div>
