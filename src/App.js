b93417d792750d1831d30b6c0e8784530441fa41
Qui Nguyen <qui@gmail.com>
2024-02-16 23:19:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 const BlogList = ({ blogs, heading }) => {
   return (
     <div className="blog-list">
       <h2>{heading}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
